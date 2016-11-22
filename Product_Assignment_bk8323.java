3f96613fd77f6ed7b6172808e28168d8b95482dd
misMobileApps <misMobileApps@gmail.com>
2016-11-22 14:58:42 -0600

    completed assignment locally

---
+++ Product_Assignment_bk8323.java
@@ -0,0 +1,8 @@
+// Create a Java class representing a Product
+// It should be based on the template here.
+// It should be named Product_Assignment_bk8323.java
+// It should have a private sku instance variable containing your eid.
+
+class Product_Assignment_bk8323 {
+	private String eid = "bk8323";
+}
